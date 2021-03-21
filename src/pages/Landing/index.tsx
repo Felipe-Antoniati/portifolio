e613722624df33b9bb614fb4b3b9f4976e3671cf
felipe-antoniati <antoniati.felipe@gmail.com>
2021-03-21 11:53:00 -0300

    middle landing: add Cards, responsive styles

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,6 +1,12 @@
 import Navbar from "../../components/Navbar";
 import Main from "../../components/Main";
 import Middle from "../../components/Middle";
+import Card from "../../components/Card";
+import SecondaryButton from "../../components/SecondaryButton";
+
+import imgHappy from "../../images/templates/happy.png";
+import imgBeachControl from "../../images/templates/beach-control.png";
+import imgBeTheHero from "../../images/templates/be-the-hero.png";
 
 import "./styles.css";
 
@@ -9,7 +15,7 @@ export default function Landing() {
     <div id="landing-page">
       <Navbar />
       <div className="fixed-content">
-        <Main 
+        <Main
           title="Felipe"
           subtitle="Antoniati"
           description="Desenvolvedor Web Freelancer, 
@@ -18,7 +24,37 @@ export default function Landing() {
         />
       </div>
       <Middle title="Veja alguns dos projetos que participei">
-        <h1>Meio</h1>
+        <div className="cards">
+          <Card
+            title="Happy"
+            description="Aplicativo para fazer a conexão entre 
+          crianças de casas sociais com pessoas 
+          que querem ajudar"
+          >
+            <img src={imgHappy} alt="Happy" />
+          </Card>
+          <Card
+            title="Controle de Praia"
+            description="Aplicativo para faciliar o controle de 
+          Acesso ao Serviço de Praia de 
+          condomínios"
+          >
+            <img src={imgBeachControl} alt="Controle de Praia" />
+          </Card>
+          <Card
+            title="Be The Hero"
+            description="Aplicativo  para fazer a conexão entre 
+          ONG's, e pessoas que querem ajudar"
+          >
+            <img src={imgBeTheHero} alt="Be The Hero" />
+          </Card>
+        </div>
+        <div className="footer-landing">
+          <SecondaryButton 
+            name="ACESSE O PORTIFOLIO"
+            path="/portifolio"
+          />
+        </div>
       </Middle>
     </div>
   );
