d63ecf5c3c5b5271eac29a35b69ea6c7250a4cce
Felipe-Antoniati <antoniati.felipe@gmail.com>
2021-03-26 13:19:11 -0300

    simplify structure / finish component Main

--- src/components/PrimaryButton/index.tsx
+++ src/components/PrimaryButton/index.tsx
@@ -10,8 +10,8 @@ interface PrimaryButtonProps {
 const PrimaryButton: 
 React.FC<PrimaryButtonProps> = ({text, path}) => {
   return(
-    <nav id="nav-btn">
-      <Link to={path} className="btn-link">
+    <nav id="primary-btn">
+      <Link to={path} className="primary-link">
         {text}
       </Link>
     </nav>
